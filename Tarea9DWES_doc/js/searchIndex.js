932bcf4b0c6c51392714eb4a2b606e84ae552994
carolpc <carolvr8@gmail.com>
2026-04-06 22:08:45 +0200

    Add HTML documentation files for Application, default, and reports
    - Created Application.html for the Application package documentation.
    - Created default.html for the main API documentation overview.
    - Added reports for deprecated elements, compilation errors, and markers with appropriate HTML structure.

---
+++ Tarea9DWES_doc/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\buscarPokemon\u0028\u0029",
+            "name": "buscarPokemon",
+            "summary": "Obtiene\u0020datos\u0020de\u0020un\u0020Pok\u00E9mon\u0020desde\u0020PokeAPI.",
+            "url": "namespaces/default.html#function_buscarPokemon"
+        },                {
+            "fqsen": "\\formatearStats\u0028\u0029",
+            "name": "formatearStats",
+            "summary": "Formatea\u0020el\u0020array\u0020de\u0020estad\u00EDsticas\u0020de\u0020un\u0020Pok\u00E9mon.",
+            "url": "namespaces/default.html#function_formatearStats"
+        },                {
+            "fqsen": "\\colorTipoPokemon\u0028\u0029",
+            "name": "colorTipoPokemon",
+            "summary": "Devuelve\u0020el\u0020color\u0020CSS\u0020asociado\u0020a\u0020un\u0020tipo\u0020de\u0020Pok\u00E9mon.",
+            "url": "namespaces/default.html#function_colorTipoPokemon"
+        },                {
+            "fqsen": "\\e\u0028\u0029",
+            "name": "e",
+            "summary": "Escapa\u0020texto\u0020para\u0020imprimirlo\u0020de\u0020forma\u0020segura\u0020en\u0020HTML.",
+            "url": "namespaces/default.html#function_e"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
